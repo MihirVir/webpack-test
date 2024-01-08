e925ef74f9705c65d2a2aef78dbc55da598ac3ce
Mihir Vir <80036612+MihirVir@users.noreply.github.com>
2024-01-08 12:07:06 +0530

    ADD: integrated cart with container

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -12,7 +12,8 @@ module.exports = {
             // It controls how webpack is going to try to decide 
             // whether or not to load that remoteEntry.js file
             remotes: {
-                products: "products@http://localhost:8000/remoteEntry.js"
+                products: "products@http://localhost:8000/remoteEntry.js",
+                cart: "cart@http://localhost:8002/remoteEntry.js"
             }
         }),
         new HtmlWebpackPlugin({
